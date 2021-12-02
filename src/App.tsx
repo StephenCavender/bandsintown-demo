cc5690d35f8760d56e29783485e805fc15e97690
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-12-01 21:44:35 -0500

    add search

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { KeyboardEvent, useEffect, useState } from 'react';
 import './App.css';
 import Hero from './components/Hero/Hero';
+import { ArtistData } from './models/artist';
+import { debounce, isEmpty } from 'lodash';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchResults, setSearchResults] = useState([]);
+  const [artist, setArtist] = useState({} as ArtistData);
+
+  const handleChange = debounce((event) => {
+    setSearchTerm(event.target.value);
+  }, 0); // debounce was for getting search suggestions...
+
+  // search api blocked by CORS
+  // useEffect(() => {
+  //   if (!searchTerm) return;
+
+  //   fetch(`https://bandsintown.com/searchSuggestions?searchTerm=${searchTerm}`)
+  //   .then((response) => response.json())
+  //   .then(({ artists }) => {
+  //     // TODO: type this map func
+  //     const names = artists.map((a: any) => a.name);
+  //     setSearchResults(names);
+  //   });
+  // }, [searchTerm])
+
+  const select = (artist: string) => {
+    fetch(`https://rest.bandsintown.com/artists/${artist.trim()}?app_id=test`)
+    .then((response) => response.json())
+    .then((result: ArtistData) => {
+      setArtist(result);
+    });
+  }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      select(searchTerm);
+    }
+  }
+
   return (
     <div className="App">
-      <Hero />
+      <div>
+        <input onChange={handleChange} type="text" placeholder="Search Artists..." onKeyDown={handleKeyDown} />
+        <button onClick={() => select(searchTerm)}>Select</button>
+      </div>
+      {/* <ul>
+         {searchResults.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul> */}
+      { isEmpty(artist) ? <h1>404 artist not found</h1> : <Hero artist={artist} /> }
     </div>
   );
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ArtistData } from '../../models/artist';
 import './Hero.css';
-import { isEmpty } from 'lodash';
 
-function Hero() {
-  const [artist, setArtist] = useState({} as ArtistData);
+function Hero(props: { artist: ArtistData }) {
 
-  useEffect(() => {
-    fetch('https://rest.bandsintown.com/artists/jasonisbell?app_id=test')
-    .then((response) => response.json())
-    .then((result: ArtistData) => {
-      setArtist(result);
-    });
-  }, [])
-
-  return isEmpty(artist) ? <p>Loading...</p> : (
-    <div className="Hero" style={{ backgroundImage: `url("${artist.image_url}")` }}>
-      <img src={artist.image_url} className="Artist-Thumb" alt="artist-thumb" />
+  return (
+    <div className="Hero" style={{ backgroundImage: `url("${props.artist.image_url}")` }}>
+      <img src={props.artist.image_url} className="Artist-Thumb" alt="artist-thumb" />
       <div className="Artist-Info">
-        <h1>{artist.name}</h1>
-        <p>{`${artist.tracker_count} Followers • ${artist.upcoming_event_count} Upcoming Concerts`}</p>
+        <h1>{props.artist.name}</h1>
+        <p>{`${props.artist.tracker_count} Followers • ${props.artist.upcoming_event_count} Upcoming Concerts`}</p>
       </div>
       <button className="Action" onClick={() => alert("suprise, I didn't implement this")}>Follow</button>
     </div>
