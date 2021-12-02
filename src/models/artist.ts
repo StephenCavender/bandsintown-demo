8952ded2bf81eeab42a0d92752e44f354054be24
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-12-01 19:55:14 -0500

    add artist model

---
+++ src/models/artist.ts
@@ -0,0 +1,56 @@
+export interface ArtistData {
+  /**
+   * 
+   * @type {number}
+   * @memberof ArtistData
+   */
+  id?: number;
+  /**
+   * 
+   * @type {string}
+   * @memberof ArtistData
+   */
+  name: string;
+  /**
+   * 
+   * @type {string}
+   * @memberof ArtistData
+   */
+  url: string;
+  /**
+   * 
+   * @type {string}
+   * @memberof ArtistData
+   */
+  imageUrl: string;
+  /**
+   * 
+   * @type {string}
+   * @memberof ArtistData
+   */
+  thumbUrl: string;
+  /**
+   * 
+   * @type {string}
+   * @memberof ArtistData
+   */
+  facebookPageUrl: string;
+  /**
+   * 
+   * @type {string}
+   * @memberof ArtistData
+   */
+  mbid: string;
+  /**
+   * 
+   * @type {number}
+   * @memberof ArtistData
+   */
+  trackerCount: number;
+  /**
+   * 
+   * @type {number}
+   * @memberof ArtistData
+   */
+  upcomingEventCount: number;
+}
